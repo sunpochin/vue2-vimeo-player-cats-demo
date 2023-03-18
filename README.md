b1541bb7d843d3faa94c8afafff727a9ff328874
pac <sunpochin@gmail.com>
2023-03-18 15:23:44 +0800

    demo of vimeo videos

---
+++ README.md
@@ -0,0 +1,26 @@
+# vimeo player demo
+Coded following this amazing video example, thanks!
+https://www.youtube.com/watch?v=VSr2T5TcBVI
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <input type="text" v-model="vimeoId">
+    <button @click="AddVimeo">Add</button>
+    <div v-for="(item, i) in vimeoIdList" :key="i">
+      <VimeoPart :id="item"></VimeoPart>
+    </div>
+  </div>
+</template>
+
+<script>
+import VimeoPart from './components/VimeoPart.vue'
+
+export default {
+  data() {
+    return {
+      vimeoId: '',
+      // 3 initial demo videos of cat and dog.
+      vimeoIdList: [807306477, 807306201, 192191]
+    }
+  },
+  methods: {
+    AddVimeo() {
+      this.vimeoIdList.push(this.vimeoId);
+    },
+  },
+  name: 'App',
+  components: {
+    VimeoPart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
