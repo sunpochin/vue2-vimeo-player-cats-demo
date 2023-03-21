f99909ca152f75c7436fd360a5b27810eb69343e
pac <sunpochin@gmail.com>
2023-03-21 15:07:31 +0800

    use youtube video

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+      <div v-for="(item, i) in youtubeIdList" :key="i">
+        <youtube :video-id="item" />
+      </div> 
+      
+
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <button @click="AddVimeo">Add a video by entering vimeo video ID</button>:
+    <!-- <button @click="AddVimeo">Add a video by entering vimeo video ID</button>:
     <input type="text" v-model="vimeoId">
     <hr>
     <div v-for="(item, i) in vimeoIdList" :key="i">
       <VimeoPart :id="item"></VimeoPart>
-    </div>
+    </div> -->
 
     <a href="https://github.com/sunpochin/vue2-vimeo-player-cats-demo">source code</a>
     <br>
@@ -16,25 +21,39 @@
 </template>
 
 <script>
-import VimeoPart from './components/VimeoPart.vue'
+// import VimeoPart from './components/VimeoPart.vue'
+import axios from 'axios'
 
 export default {
   data() {
     return {
+      youtubeIdList: ['HlFgIBAm0Jg','OgyZIZMZAUM', 'UovBEFOgidI'],
+
       vimeoId: '',
       // 3 initial demo videos of cat and dog.
       vimeoIdList: ['807306477', '807306201', '192191']
     }
   },
+  mounted() {
+    console.log('mounted')
+    axios
+      .get('http://localhost:8000/tube?q=餵食')
+      // .get('https://nodejs-video-api.onrender.com/tube?q=餵食')
+      .then(response => {
+        console.log('first: ' + response)
+        console.log('data: ' + response.data)
+      } )
+  },
+
   methods: {
     AddVimeo() {
       this.vimeoIdList.push(this.vimeoId);
     },
   },
   name: 'App',
-  components: {
-    VimeoPart
-  }
+  // components: {
+  //   VimeoPart
+  // }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueYoutube from 'vue-youtube';
+Vue.use(VueYoutube);
+
+import cors from 'cors';
+Vue.use(cors);
+
 Vue.config.productionTip = false
 
 new Vue({
